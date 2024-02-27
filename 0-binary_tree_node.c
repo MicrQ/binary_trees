4423829beabba855b68b7c1d47f11470d1167401
MicrQ <mail.micrq@proton.me>
2024-02-27 15:02:59 +0300

    betty style checked

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,10 +2,10 @@
 
 /**
  * binary_tree_node - a function that creates a binary tree.
- * 
+ *
  * @parent: the parent of the newly created node.
  * @value: the data for the newly created node.
- * 
+ *
  * Return: the newly created nodes address or NULL on failure.
 */
 
@@ -13,8 +13,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-if (!node)
-    return (NULL);
+	if (!node)
+		return (NULL);
 
 	node->n = value;
 	node->left = NULL;
