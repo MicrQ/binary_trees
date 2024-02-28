e7a3240a21c24398bd1dee77b61e2baac181e78e
MicrQ <mail.micrq@proton.me>
2024-02-28 15:05:59 +0300

    testing task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,53 @@
+#include "binary_trees.h"
+
+size_t max(size_t arg1, size_t arg2);
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/**
+ * binary_tree_balance - a function that measures the balance factor
+ *			of a binary tree.
+ * @tree: the root of the tree.
+ * Return: the balance factor.
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int balance;
+
+	if (!tree)
+		return (0);
+	balance = (int)binary_tree_height(tree->left)
+		- (int)binary_tree_height(tree->right);
+	return (balance);
+}
+
+
+/**
+ * binary_tree_height - a function that measures the height of a binary tree.
+ * @tree: a pointer to the root of the tree.
+ * Return: the height of the tree.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	return (max(binary_tree_height(tree->left),
+			binary_tree_height(tree->right)) + 1);
+}
+
+/**
+ * max - a function that returns the maximum of the two arguments.
+ *
+ * @arg1: value 1
+ * @arg2: value 2
+ *
+ * Return: the maximum.
+ */
+
+size_t max(size_t arg1, size_t arg2)
+{
+	if (arg1 > arg2)
+		return (arg1);
+	return (arg2);
+}
