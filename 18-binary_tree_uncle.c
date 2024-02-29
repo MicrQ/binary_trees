f9a40c2333d699c60a805bf28bbf9cd491b21bd3
MicrQ <mail.micrq@proton.me>
2024-02-29 15:59:10 +0300

    MODIFIED: a function that finds the uncle of a given node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,7 +8,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node && node->parent->parent)
+	if (node && node->parent && node->parent->parent)
 		return ((node->parent == node->parent->parent->right)
 				? node->parent->parent->left : node->parent->parent->right);
 	return (NULL);
