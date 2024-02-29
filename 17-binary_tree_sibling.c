a068e7b7217e54b96e88c9e357767dbb2302a22b
MicrQ <mail.micrq@proton.me>
2024-02-29 13:56:46 +0300

    MODIFIED: to check the node instade of the node data:  a function that finds the sibling of a given tree node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node && node->parent)
 	{
-		return ((node->n == node->parent->right->n)
+		return ((node == node->parent->right)
 				? node->parent->left : node->parent->right);
 	}
 	return (NULL);
